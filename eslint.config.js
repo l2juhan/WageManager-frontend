19e3f4a490b059516b2693398b0948e92d1816ff
l2juhan <leejuhan0527@gmail.com>
2025-11-13 22:58:03 +0900

    chore: ESLint 설정 추가 및 React 플러그인 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -10,11 +11,12 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -22,7 +24,19 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
+      // React 17 이상에서는 import React from 'react' 불필요
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      // 대문자로 시작하는 컴포넌트는/아이콘은 unused 검사에서 제외
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
