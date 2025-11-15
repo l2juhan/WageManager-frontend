6633d5704f3d8a36d5ed6d045f2c5226082ae41e
l2juhan <80254217+l2juhan@users.noreply.github.com>
2025-11-15 18:01:45 +0900

    Revert "파일 생성 - from upstream"

--- src/pages/workers/MyPage.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function MyPage() {
-  return (
-  <div className="worker-content-frame">
-    마이페이지 (더미)
-  </div>
-  );
-}
--- src/pages/workers/RemittancePage.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function RemittancePage() {
-  return (
-  <div className="worker-content-frame">
-    송금 관리 페이지 (더미)
-  </div>
-  );
-}
